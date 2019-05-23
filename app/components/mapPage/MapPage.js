04ae1aea2f713b70ae45c8dfb87d830dfaacd282
MacBook <macair@MacBook-Air-Mac.local>
2019-05-23 18:38:43 +0300

    добавил redux

--- app/components/mapPage/MapPage.js
+++ app/components/mapPage/MapPage.js
@@ -1,24 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class MapPage extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -33,16 +18,16 @@ export default class App extends Component<Props> {
            }}
          >
          </MapView>
-         <Text>Hello world</Text>
+         <Text>HI</Text>
      </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
    ...StyleSheet.absoluteFillObject,
-   height: 400,
+   height: 800,
    width: 400,
    justifyContent: 'flex-end',
    alignItems: 'center',
---
+++ app/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { MarkerList } from './markerList/MarkerList';
+
+export default combineReducers({
+  MarkerList,
+})
